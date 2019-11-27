c2e2109c6950a94d6aa1b8394aab7cf252861d41
maxie7 <maxie7@protonmail.com>
2019-11-27 20:53:40 +0100

    Updates

---
+++ precache-manifest.5d6b44f589623425358f5c03b4b4205b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f559e8fa3719cce85e8f9a0461d2ca20",
+    "url": "/monsters-robots/index.html"
+  },
+  {
+    "revision": "7b5bc42466a864d02eba",
+    "url": "/monsters-robots/static/css/main.af5e0322.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-robots/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "7b5bc42466a864d02eba",
+    "url": "/monsters-robots/static/js/main.8b52851c.chunk.js"
+  },
+  {
+    "revision": "51af22b2dc0a607ec980",
+    "url": "/monsters-robots/static/js/runtime-main.fd94ea9f.js"
+  }
+]);
