e8f2167ed95b998d244a6d494e197ae129c7906c
MaxKos <maxie7@protonmail.com>
2019-11-27 09:31:18 +0100

    Add first component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
@@ -7,29 +9,25 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          id: 'asc1',
-          name: 'Frankenstein'
-        },
-        {
-          id: 'asc2',
-          name: 'Dracula'
-        },
-        {
-          id: 'asc3',
-          name: 'Zombie'
-        },
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map( monster => <h1 key={monster.id}>{ monster.name }</h1>)
-        }
+        <CardList name='Max'>
+          <h1>Max</h1>
+        </CardList>
+        {this.state.monsters.map( monster => (
+          <h1 key={monster.id}> { monster.name } </h1>
+        ))}
       </div>
     )
   }
